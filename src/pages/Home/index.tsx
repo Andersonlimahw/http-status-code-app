54f396b874931e218fda6bc392f7beee1a9a95b8
Anderson Lima <andersonlimahw@gmail.com>
2023-05-01 23:05:03 -0300

    🆕 [feature] [Imagem do card]

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -45,10 +45,10 @@ export default function Home() {
       </div>
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {httpListZustand.httpList.map((item: ItemModel, index: number) => (
+          {httpListZustand.httpList.map((item: ItemModel) => (
             <Card key={item.id} item={{
               ...item,
-              imageUrl: item.imageUrl.replace('?1', `?${index}`)
+              imageUrl: item.imageUrl
             }} handleClick={() => handleClick(item)} />
           ))}
         </div>
